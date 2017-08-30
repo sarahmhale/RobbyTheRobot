95d3516706dffa99e0fa901fe1512b2043e408cb
ie09hhm <heho0017@student.umu.se>
2017-08-30 16:28:03 +0200

    working on file structure

---
+++ Calculations/Distance/distance.py
@@ -0,0 +1,46 @@
+"""
+Function name: getCordinatesWithinRange
+Purpose:
+Input: path, rangeValue
+Output: a list of all cordinates within range sorted by furthest distance first.
+Comments:
+
+
+
+cordinate = [x, y]
+
+list cordinatesWithinRange
+bool inRange = true
+int index
+while(inRange):
+	
+	 if getDistance(path[index]) <= rangeValue
+		cordinatesWithinRange.append.[cordinate, length]
+		cordinatesWithinRange.sort(1)
+		
+return sortByLength(cordinatesWithinRange)
+"""
+
+"""
+Function name: getDistance
+Purpose: get distance between two coordinates
+Input: [x,y] , [x,y]
+Output: distance between the coordinates as a double
+Comments: används av getCordinatesWithinRange
+"""
+
+"""
+Function name:
+Purpose:
+Input:
+Output:
+Comments: används av getCordinatesWithinRange
+"""
+
+"""
+Function name:
+Purpose:
+Input:
+Output:
+Comments:
+"""
