64d3fccc9747b5c543d2c8e520bbc817f0eb7f0f
id15she <id15she@cs.umu.se>
2017-09-04 10:37:58 +0200

    added getPos and postSpeed

--- Position/Position.py
+++ Position/Position.py
@@ -1,6 +1,36 @@
+MRDS_URL = 'localhost:50000'
+
+import http.client, json, time
 from collections import namedtuple
+
+HEADERS = {"Content-type": "application/json", "Accept": "text/json"}
 
 def createPosition(x, y):
     Position = namedtuple('Position', ['x', 'y'])
     p = Position(x=x, y=y)
     return p
+
+def getPose():
+    """Reads the current position and orientation from the MRDS"""
+    mrds = http.client.HTTPConnection(MRDS_URL)
+    mrds.request('GET','/lokarria/localization')
+    response = mrds.getresponse()
+    if (response.status == 200):
+        poseData = response.read()
+        response.close()
+        return json.loads(poseData.decode())
+    else:
+        return UnexpectedResponse(response)
+    
+def postSpeed(angularSpeed,linearSpeed):
+    """Sends a speed command to the MRDS server"""
+    mrds = http.client.HTTPConnection(MRDS_URL)
+    params = json.dumps({'TargetAngularSpeed':angularSpeed,'TargetLinearSpeed':linearSpeed})
+    mrds.request('POST','/lokarria/differentialdrive',params,HEADERS)
+    response = mrds.getresponse()
+    status = response.status
+    #response.close()
+    if status == 204:
+        return response
+    else:
+        raise UnexpectedResponse(response)    
